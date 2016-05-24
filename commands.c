3f85a6fc2fe3d74740a0efb034c8285e1a2cf32c
Jesse Frey <jmfrey@alaska.edu>
2016-05-24 15:27:15 -0800

    Added example command to show how command arguments function

--- commands.c
+++ commands.c
@@ -10,9 +10,32 @@
 #include "pins.h"
 
 
+int example_command(char **argv,unsigned short argc){
+  int i,j;
+  printf("This is an example command that shows how arguments are passed to commands.\r\n""The values in the argv array are as follows : \r\n");
+  for(i=0;i<=argc;i++){
+    printf("argv[%i] = 0x%p\r\n\t""string = \"%s\"\r\n",i,argv[i],argv[i]);
+    //print out the string as an array of hex values
+    j=0;
+    printf("\t""hex = {");
+    do{
+      //check if this is the first element
+      if(j!='\0'){
+        //print a space and a comma
+        printf(", ");
+      }
+      //print out value
+      printf("0x%02hhX",argv[i][j]);
+    }while(argv[i][j++]!='\0');
+    //print a closing bracket and couple of newlines
+    printf("}\r\n\r\n");
+  }
+  return 0;
+}
 
 //table of commands with help
 const CMD_SPEC cmd_tbl[]={{"help"," [command]",helpCmd},
+                   {"ex","[arg1] [arg2] ...\r\n\t""Example command to show how arguments are passed",example_command},
                    ARC_COMMANDS,CTL_COMMANDS,ERROR_COMMANDS,
                    //end of list
                    {NULL,NULL,NULL}};
