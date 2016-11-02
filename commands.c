db4bd869443c0804c10c195414f55df1853e5029
unknown <cjodell2@SSEP-5.ua.ad.alaska.edu>
2016-11-01 16:54:05 -0800

    started arc2 build of barebones

--- commands.c
+++ commands.c
@@ -1,3 +1,10 @@
+//===============================================
+// MODIFIED AND DESKTOP VERSION
+// THIS VERSION SHOULD NOT BE IN THE Z DRIVE
+// THIS IS SOLELY FOR TESTING PURPOSES
+// ASYNC IMG
+//===============================================
+
 #include <msp430.h>
 #include <stdio.h>
 #include <string.h>
@@ -8,7 +15,6 @@
 #include <ARCbus.h>
 #include <stdlib.h>
 #include "pins.h"
-
 
 int example_command(char **argv,unsigned short argc){
   int i,j;
@@ -33,10 +39,162 @@ int example_command(char **argv,unsigned short argc){
   return 0;
 }
 
+int LED(char **argv, unsigned short argc)
+{
+  printf("Turning LED on\r\n");
+  P7OUT=BIT0;
+  printf("TUrning LED off\r\n");
+  
+  return 0;
+}
+
+/*
+int VidOn(char **argv, unsigned short argc)
+{
+  printf("Turning video on\r\n");
+  if(Adafruit_VC0706_TVon())
+  {
+      printf("Video on\r\n");
+      return 0;
+  }else
+  {
+      printf("Error turning video on\r\n");
+      return 1;
+  }
+}
+int VidOff(char **argv, unsigned short argc)
+{
+  printf("Turning video off\r\n");
+  if(Adafruit_VC0706_TVoff())
+  {
+      printf("Video off\r\n");
+      return 0;
+  }else
+  {
+      printf("Error turning video off\r\n");
+      return 1;
+  }
+}
+
+int SDcard(char ** argv, unsigned short argc)
+{
+  printf("Initializing SD card\r\n");
+  if(mmcInit_card())
+  {
+      printf("SD card initialized\r\n");
+      return 0;
+  }
+  else
+  {
+      printf("Error initializing SD card\r\n");
+      return 1;
+  }
+}
+*/
+
+//ARC_ASYNC_PROXY_COMMAND found in Z:\ARC-2\ARC-2-Software\Libraries\commands\ARCbus_commands.c 
+//Approximately starting line 189
+
+//connect to a remote board via async
+int asyncProxyCmd(char **argv,unsigned short argc)
+{
+  //internal events for asyncProxy command
+  enum{ASYNC_CMD_EV_RX_ASYNC=1<<0,ASYNC_CMD_EV_RX_UART=1<<1,ASYNC_CMD_EV_CLOSE_ASYNC=1<<2,ASYNC_CMD_EV_ALL=(ASYNC_CMD_EV_RX_ASYNC|ASYNC_CMD_EV_RX_UART|ASYNC_CMD_EV_CLOSE_ASYNC)};
+  char c;
+  int err;
+  CTL_EVENT_SET_t e=0,evt;
+  unsigned char addr;
+  unsigned char buff[BUS_I2C_CRC_LEN+1+BUS_I2C_HDR_LEN],*ptr;
+  if(argc>1)
+  {
+  printf("Error : %s takes 0 or 1 arguments\r\n",argv[0]);
+  return -1;
+  }
+  if(argc==1)
+  {
+    addr=getI2C_addr(argv[1],0,busAddrSym);
+    if(addr==0xFF)
+    {
+      return -1;
+    }
+    //print out address
+    printf("Using Address 0x%02X\r\n",addr);
+    //check if running with CDH lib
+    //try to open async connection
+    if((err=async_open(addr)))
+    {
+      //print error
+      printf("Error : opening async\r\n%s\r\n",BUS_error_str(err));
+      //return error
+      return -2;
+    }
+    //Tell the user that async is open
+    printf("async open use ^C to force quit\r\n");
+    //setup events
+    async_setup_events(&e,0,ASYNC_CMD_EV_RX_ASYNC);
+    UCA1_setup_events(&e,0,ASYNC_CMD_EV_RX_UART);
+    async_setup_close_event(&e,ASYNC_CMD_EV_CLOSE_ASYNC);
+    for(;;)
+    {
+      //wait for event
+      evt=ctl_events_wait(CTL_EVENT_WAIT_ANY_EVENTS,&e,ASYNC_CMD_EV_ALL,CTL_TIMEOUT_NONE,0);
+      //check for char from UART
+      if(evt&ASYNC_CMD_EV_RX_UART){
+        //get char
+        c=UCA1_Getc();
+        //check for ^C
+        if(c==0x03){
+          //close connection
+          async_close();
+          //print message
+          printf("\r\nConnection terminated by user\r\n");
+          //exit loop
+          break;
+        }
+        //send char over async
+        async_TxChar(c);
+      }
+      //check for char from async
+      if(evt&ASYNC_CMD_EV_RX_ASYNC){
+        //get char from async
+        c=async_Getc(); 
+        //print char to UART
+        UCA1_TxChar(c);
+      }
+      if(evt&ASYNC_CMD_EV_CLOSE_ASYNC){
+        //print message
+        printf("\r\nconnection closed remotely\r\n");
+        //exit loop
+        break;
+      }
+    }
+    //stop monitoring events
+    async_setup_events(NULL,0,0);
+    UCA1_setup_events(NULL,0,0);
+    async_setup_close_event(NULL,0);
+  }
+  else{
+    //close async
+    err=async_close();
+    //check for error
+    if(err){
+      //print error
+      printf("Error : async_close() failed %s\r\n",BUS_error_str(err));
+      return -4;
+    }
+  }
+  return 0;
+}
+
 //table of commands with help
 const CMD_SPEC cmd_tbl[]={{"help"," [command]",helpCmd},
-                   {"ex","[arg1] [arg2] ...\r\n\t""Example command to show how arguments are passed",example_command},
-                   ARC_COMMANDS,CTL_COMMANDS,ERROR_COMMANDS,
+                   //ARC_ASYNC_PROXY_COMMAND,
+                   {"ex", "[arg1] [arg2] ...\r\n\t""Example command to show how arguments are passed",example_command},
+                   {"VidOn", "[arg1] [arg2] ...\r\n\t" "Turn camera video on", VidOn},
+                   {"VidOff", "[arg1] [arg2] ...\r\n\t" "Turn camera video off", VidOff},
+                   {"SDcard", "[arg1] [arg2] ...\r\n\t" "Initialize SD card", SDcard},
+                   {"async", "[arg1] [arg2] ...\r\n\t" "Initialize async for IMG", asyncProxyCmd},
+                   //ARC_COMMANDS,CTL_COMMANDS,ERROR_COMMANDS,
                    //end of list
                    {NULL,NULL,NULL}};
 
--- main.c
+++ main.c
@@ -1,11 +1,35 @@
+//===============================================
+// MODIFIED AND DESKTOP VERSION
+// THIS VERSION SHOULD NOT BE IN THE Z DRIVE
+// THIS IS SOLELY FOR TESTING PURPOSES
+// ASYNC IMG
+//===============================================
+
 #include <msp430.h>
 #include <ctl.h>
 #include <ARCbus.h>
 #include <Error.h>
 #include <terminal.h>
-#include <UCA1_uart.h>
+#include <UCA3_uart.h>
+#include "SDlib.h"
 #include "pins.h"
 #include "subsystem.h"
+//#include "sensor.h"
+
+//==============================================================
+
+  #define saved_settings   ((FLASH_SETTINGS*)0x01000)
+  
+  #define SAVED_SETTINGS_MAGIC        0xA45F
+  typedef struct{
+    unsigned short magic;
+    //address for bus communication
+    unsigned char addr;
+    //baud rate settings
+    unsigned char br0,br1,mctl,clk;
+  } FLASH_SETTINGS;
+  
+//==============================================================
 
 CTL_TASK_t terminal_task,sub_task;
 
@@ -15,33 +39,60 @@ unsigned terminal_stack[2000];
 unsigned sub_stack[1000];
 
 //make printf and friends use async
-int __putchar(int c){
-  return UCA1_TxChar(c);
+int __putchar(int c)
+{
+  return UCA3_TxChar(c);
 }
 
 //make printf and friends use async
-int __getchar(void){
-  return UCA1_Getc();
+int __getchar(void)
+{
+  return UCA3_Getc();
 }
 
+void main(void)
+{
+unsigned char addr;
+  //DO this first
+  ARC_setup(); 
 
-
-void main(void){
   //turn on LED's this will flash the LED's during startup
   P7OUT=0xFF;
   P7DIR=0xFF;
-  //DO this first
-  ARC_setup(); 
+
+  //Initialize camera sensor
+  sensor_init();
+
   //TESTING: set log level to report everything by default
-  set_error_level(0);
+  //set_error_level(0);
+
   //initialize UART
-  UCA1_init_UART(UART_PORT,UART_TX_PIN_NUM,UART_RX_PIN_NUM);
+  UCA3_init_UART(UART_PORT,UART_TX_PIN_NUM,UART_RX_PIN_NUM);
+  UCA3_BR57600();
 
+  if(saved_settings->magic==SAVED_SETTINGS_MAGIC)
+  {
+    //check if address is valid
+    if(addr&0x7F)
+    {
+      //get address
+      addr=saved_settings->addr;
+    }
+       
+  }
   //setup bus interface
-  initARCbus(0x1F);
-  //init complete turn on LED0 and all others off
-  P7OUT=BIT0;
+  initARCbus(addr);
+  
+  mmcInit_msp();
 
+ //initialize stacks
+ /*
+  memset(terminal_stack,0xcd,sizeof(terminal_stack));  // write known values into the stack
+  terminal_stack[0]=terminal_stack[sizeof(terminal_stack)/sizeof(terminal_stack[0])-1]=0xfeed; // put marker values at the words before/after the stack
+  
+  memset(sub_stack,0xcd,sizeof(sub_stack));  // write known values into the stack
+  sub_stack[0]=sub_stack[sizeof(sub_stack)/sizeof(sub_stack[0])-1]=0xfeed; // put marker values at the words before/after the stack
+   */ 
   ctl_task_run(&terminal_task,BUS_PRI_LOW,terminal,"Example Bare Bones program ready","terminal",sizeof(terminal_stack)/sizeof(terminal_stack[0])-2,terminal_stack-1,0);
   ctl_task_run(&sub_task,BUS_PRI_HIGH,sub_events,NULL,"SUB_events",sizeof(sub_stack)/sizeof(sub_stack[0])-2,sub_stack-1,0);
 
@@ -49,9 +100,5 @@ void main(void){
   mainLoop(); 
 }
 
-//decode errors
-char *err_decode(char buf[150], unsigned short source,int err, unsigned short argument){
-  sprintf(buf,"source = %i, error = %i, argument = %i",source,err,argument);
-  return buf;
-}
+
 
